f8dcda955172ba907a7ab600fa21426baa359428
mandalmrinmoy <112158728+mandalmrinmoy@users.noreply.github.com>
2024-05-30 23:39:45 +0530

    Add files via upload

---
+++ 07_practice.py
@@ -0,0 +1,20 @@
+# coffee customization
+coffee=input("Please select coffee size: ")
+coffee_size=coffee.lower()
+shot=input("Please select extra shot: ")
+extra_shot=shot.lower()
+if coffee_size=="small":
+    if extra_shot=="yes":
+        print("Ready!! your coffee ",coffee_size,"sizen with extra shot")
+    else:
+        print("Ready!! your coffee ",coffee_size,"size")
+elif coffee_size=="medium":
+    if extra_shot=="yes":
+        print("Ready!! your coffee ",coffee_size,"sizen with extra shot")
+    else:
+        print("Ready!! your coffee ",coffee_size,"size")
+elif coffee_size=="large":
+    if extra_shot=="yes":
+        print("Ready!! your coffee ",coffee_size,"sizen with extra shot")
+    else:
+        print("Ready!! your coffee ",coffee_size,"size")
